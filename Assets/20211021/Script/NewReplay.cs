a94cbcc78477a141d6e8217faf485e57af9a9d44
steidl90 <87983461+steidl90@users.noreply.github.com>
2021-11-03 22:28:18 +0900

    리플레이 테스트

--- Assets/20211021/Script/NewReplay.cs
+++ Assets/20211021/Script/NewReplay.cs
@@ -5,47 +5,40 @@ using UnityEngine;
 public class NewReplay : MonoBehaviour
 {
     public List<ReplayData> data;
-    
+    private Vector3 pos;
+    private Quaternion rot;
+
     private void Start()
     {
         data = new List<ReplayData>();
     }
 
-    //private void Update()
-    //{
-    //    if (data.Count != 0)
-    //    {
-    //        ReplayData player = data[data.Count - 1];
-    //        switch (player.types)
-    //        {
-    //            case ReplayData.Types.active:
-    //                player.target.SetActive(true);
-    //                break;
-    //            case ReplayData.Types.posAndRot:
-    //                if (player.target ==
-    //                    GetComponent<CreatRagdoll>().originalRagdoll.GetComponent<Ragdoll>().ragdoll.gameObject)
-    //                {
-    //                    player.target = GetComponent<CreatRagdoll>().replayRagdoll.GetComponent<Ragdoll>().ragdoll.gameObject;
+    private void Update()
+    {
+        var isReplay = GameManager.gameManager.player.GetComponent<PlayerController>().IsReplay;
+        if (data.Count != 0 && isReplay)
+        {
+            ReplayData player = data[data.Count - 1];
+            switch (player.types)
+            {
+                case ReplayData.Types.posAndRot:
+                    if (player.target ==
+                        GetComponent<CreatRagdoll>().originalRagdoll.GetComponent<Ragdoll>().ragdoll.gameObject)
+                    {
+                        player.target = GetComponent<CreatRagdoll>().replayRagdoll.GetComponent<Ragdoll>().ragdoll.gameObject;
 
-    //                    player.target.transform.position = player.position;
-    //                    player.target.transform.rotation = player.rotation;
-    //                }
-    //                else if (player.target == GameObject.FindGameObjectWithTag("PlayerModel"))
-    //                {
-    //                    player.target.GetComponent<Rigidbody>().MovePosition(player.position);
-    //                    player.target.GetComponent<Rigidbody>().MoveRotation(player.rotation);
-    //                }
-    //                break;
-    //            case ReplayData.Types.deActive:
-    //                player.target.transform.position = new Vector3(player.target.transform.position.x, player.target.transform.position.y - 1.5f, player.target.transform.position.z);
-    //                GameManager.gameManager.cameraManager.GetComponent<CameraManager>().sub.GetComponent<FollowTarget>().pos = player.target.transform.position;
-    //                player.target.GetComponent<Rigidbody>().isKinematic = true;
-    //                player.target.SetActive(false);
-    //                break;
-    //        }
-    //        data.RemoveAt(data.Count - 1);
-    //    }
-    //}
+                        player.target.transform.position = Vector3.Lerp(player.target.transform.position, pos, Time.deltaTime * 10f);
+                    }
+                    else if (player.target == GameObject.FindGameObjectWithTag("PlayerModel"))
+                    {
+                        player.target.transform.position = Vector3.Lerp(player.target.transform.position, pos, Time.deltaTime * 10f);
+
+                    }
+                    break;
+            }
+            data.RemoveAt(data.Count - 1);
+        }
+    }
 
     public void OnReplay()
     {
@@ -63,12 +56,13 @@ public class NewReplay : MonoBehaviour
                     { 
                         player.target = GetComponent<CreatRagdoll>().replayRagdoll.GetComponent<Ragdoll>().ragdoll.gameObject;
 
-                        player.target.transform.position = player.position;
+                        pos = player.position;
                         player.target.transform.rotation = player.rotation;
+
                     }
                     else if(player.target == GameObject.FindGameObjectWithTag("PlayerModel"))
                     {
-                        player.target.GetComponent<Rigidbody>().MovePosition(player.position);
+                        pos = player.position;
                         player.target.GetComponent<Rigidbody>().MoveRotation(player.rotation);
                     }
                     break;
@@ -79,7 +73,6 @@ public class NewReplay : MonoBehaviour
                     player.target.SetActive(false);
                     break;
             }
-            data.RemoveAt(data.Count - 1);
         }
     }
 
